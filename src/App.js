6561c0fcbe162603674e0dd088d587661935e086
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-02-09 16:40:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+// import { popup, CommentForm } from './components/popup-window.js';
+import popup from './components/popup-window.js';
 
 const App = () => {
+  document.body.appendChild(popup());
+  // document.body.appendChild(CommentForm());
   document.body.appendChild(Header());
   document.body.appendChild(Footer());
 };
